0c8531adb4acd55134cbb7cf7569ed93e5e12091
RafaelMonteiro95 <rafaelmonteiro95@gmail.com>
2017-01-10 18:03:35 -0200

    created disciplines core files

--- modules/main.py
+++ modules/main.py
@@ -2,8 +2,13 @@ from WordEmbedding import *
 
 f1 = open('1.txt');
 f2 = open('2.txt');
+f3 = open('nucleos/matematica.txt');
+f4 = open('nucleos/profissional.txt')
 
 a = wordEmbedding(f1.read());
 b = wordEmbedding(f2.read());
+c = wordEmbedding(f3.read());
+d = wordEmbedding(f4.read());
 
-print(a.compare(b));
+print(c.compare(a));
+print(d.compare(a));
