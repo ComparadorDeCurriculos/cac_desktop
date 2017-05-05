d6f11e242116d70c4ccc8b4509ebbbd8f90039d4
rafaelmonteiro95 <rafaelmonteiro95@gmail.com>
2017-05-05 11:19:48 -0300

    added BCC_UFRGS

--- modules/main.py
+++ modules/main.py
@@ -6,9 +6,11 @@ if __name__ == '__main__':
 	sbc = Course("Referência", "SBC", "nucleos/computacao_ref.txt")
 	icmc = Course("BCC", "ICMC", "cursos/BCC_ICMC.txt", sbc)
 	ime = Course("BCC", "IME", "cursos/BCC_IME.txt", sbc)
+	ufrgs = Course("BCC", "UFRGS", "cursos/BCC_IME.txt", sbc)
 
 	#gera um grafico 
 	plt.plotBarCores('result.pdf', icmc.getCoresCreditsDict());
 	plt.plotBarCores('result2.pdf', ime.getCoresCreditsDict());
+	plt.plotBarCores('result3.pdf', ufrgs.getCoresCreditsDict());
 
-	plt.plotVenn(icmc.compare(ime, 0.15), "venn.pdf");
+	plt.plotVenn(icmc.compare(ufrgs, 0.15), "venn.pdf");
